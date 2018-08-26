196a37c3f1053140c7019a1bbdc28c2a5e0ffba5
xuming06 <507153809@qq.com>
2018-08-26 20:14:10 +0800

    update tts.

--- parrots/tts.py
+++ parrots/tts.py
@@ -21,73 +21,11 @@ from parrots.num_util import num2chinese
 class TextToSpeech(object):
     def __init__(self, syllables_path='./data/syllables'):
         self.syllables_path = syllables_path
+        # TODO: 分数的读法 2.11 待修复，如何添加'.'
         self.punctuation = ['，', '。', '？', '！', '“', '”', '；', '：', '（', '）',
-                            ':', ';', ',', '.', '?', '!', '\"', "\'", '(', ')']
+                            '.', ':', ';', ',', '?', '!', '\"', "\'", '(', ')']
 
-    def speak(self, text):
-        syllables = lazy_pinyin(text, style=pypinyin.TONE3)
-        print(syllables)
-        delay = 0
-
-        def preprocess(syllables):
-            temp = []
-            for syllable in syllables:
-                for p in self.punctuation:
-                    syllable = syllable.replace(p, "")
-                if syllable.isdigit():
-                    syllable = num2chinese(syllable)
-                    new_sounds = lazy_pinyin(syllable, style=pypinyin.TONE3)
-                    for e in new_sounds:
-                        temp.append(e)
-                else:
-                    temp.append(syllable)
-            return temp
-
-        syllables = preprocess(syllables)
-        for syllable in syllables:
-            path = os.path.join(self.syllables_path, syllable + ".wav")
-            _thread.start_new_thread(TextToSpeech.play_audio, (path, delay))
-            delay += 0.355
-
-    def synthesize(self, text, src, dst):
-        """
-        Synthesize .wav from text
-        src is the folder that contains all syllables .wav files
-        dst is the destination folder to save the synthesized file
-        """
-        print("Synthesizing ...")
-        delay = 0
-        increment = 355  # milliseconds
-        pause = 500  # pause for punctuation
-        syllables = lazy_pinyin(text, style=pypinyin.TONE3)
-
-        # initialize to be complete silence, each character takes up ~500ms
-        result = AudioSegment.silent(duration=500 * len(text))
-        for syllable in syllables:
-            path = src + syllable + ".wav"
-            print(path)
-            sound_file = Path(path)
-            # insert 500 ms silence for punctuation marks
-            if syllable in self.punctuation:
-                short_silence = AudioSegment.silent(duration=pause)
-                result = result.overlay(short_silence, position=delay)
-                delay += increment
-                continue
-            # skip sound file that doesn't exist
-            if not sound_file.is_file():
-                continue
-            segment = AudioSegment.from_wav(path)
-            result = result.overlay(segment, position=delay)
-            delay += increment
-
-        directory = dst
-        if not os.path.exists(directory):
-            os.makedirs(directory)
-
-        result.export(directory + "generated.wav", format="wav")
-        print("Exported.")
-
-    def play_audio(path, delay, chunk=1024):
+    def _play_audio(self, path, delay, chunk=1024):
         try:
             time.sleep(delay)
             wf = wave.open(path, 'rb')
@@ -112,3 +50,61 @@ class TextToSpeech(object):
             print(ioe)
         except Exception as e:
             print(e)
+
+    def speak(self, text):
+        syllables = lazy_pinyin(text, style=pypinyin.TONE3)
+        print(syllables)
+        delay = 0
+
+        def preprocess(syllables):
+            temp = []
+            for syllable in syllables:
+                for p in self.punctuation:
+                    syllable = syllable.replace(p, '')
+                if syllable.isdigit():
+                    syllable = num2chinese(syllable)
+                    new_sounds = lazy_pinyin(syllable, style=pypinyin.TONE3)
+                    for e in new_sounds:
+                        temp.append(e)
+                else:
+                    temp.append(syllable)
+            return temp
+
+        syllables = preprocess(syllables)
+        for syllable in syllables:
+            path = os.path.join(self.syllables_path, syllable + ".wav")
+            _thread.start_new_thread(self._play_audio, (path, delay))
+            delay += 0.355
+
+    def synthesize(self, input_text='', output_wav_path='./'):
+        """
+        Synthesize .wav from text
+        input_text: the folder that contains all syllables .wav files
+        output_wav_path: the destination folder to save the synthesized file
+        """
+        print("Synthesizing ...")
+        delay = 0
+        increment = 355  # milliseconds
+        pause = 500  # pause for punctuation
+        syllables = lazy_pinyin(input_text, style=pypinyin.TONE3)
+
+        # initialize to be complete silence, each character takes up ~500ms
+        result = AudioSegment.silent(duration=500 * len(input_text))
+        for syllable in syllables:
+            path = os.path.join(self.syllables_path, syllable + ".wav")
+            sound_file = Path(path)
+            # insert 500 ms silence for punctuation marks
+            if syllable in self.punctuation:
+                short_silence = AudioSegment.silent(duration=pause)
+                result = result.overlay(short_silence, position=delay)
+                delay += increment
+                continue
+            # skip sound file that doesn't exist
+            if not sound_file.is_file():
+                continue
+            segment = AudioSegment.from_wav(path)
+            result = result.overlay(segment, position=delay)
+            delay += increment
+
+        result.export(output_wav_path, format="wav")
+        print("Exported:" + output_wav_path)
---
+++ tests/tts_synthesize_test.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+@author:XuMing（xuming624@qq.com)
+@description: 
+"""
+from parrots.tts import TextToSpeech
+
+if __name__ == '__main__':
+    tts = TextToSpeech(syllables_path='../parrots/data/syllables')
+    print('中文语言合成')
+    while True:
+        tts.synthesize(input_text=input('输入中文：'), output_wav_path='./out.wav')
