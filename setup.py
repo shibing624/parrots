744df415bd20755465f70816c837eb173f3fef7f
shibing624 <shibing624@126.com>
2024-02-13 01:42:07 +0800

    update version.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('README.md', 'r', encoding='utf-8') as f:
 
 setup(
     name='parrots',
-    version='0.2.0',
+    version='1.0.1',
     description='Parrots, Automatic Speech Recognition(**ASR**), Text-To-Speech(**TTS**) toolkit',
     long_description=readme,
     long_description_content_type='text/markdown',
