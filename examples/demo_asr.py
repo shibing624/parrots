f359a3d64b15183615deaec22d31613fa4ed6c0d
shibing624 <shibing624@126.com>
2025-10-29 22:26:59 +0800

    update asr

--- examples/demo_asr.py
+++ examples/demo_asr.py
@@ -3,7 +3,6 @@
 @author:XuMing(xuming624@qq.com)
 @description: 
 """
-import argparse
 import os
 import sys
 
@@ -13,11 +12,6 @@ from parrots import SpeechRecognition
 pwd_path = os.path.abspath(os.path.dirname(__file__))
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--model_name_or_path', default='BELLE-2/Belle-distilwhisper-large-v2-zh', type=str)
-    args = parser.parse_args()
-    print(args)
-
-    m = SpeechRecognition(model_name_or_path=args.model_name_or_path)
+    m = SpeechRecognition(model_name_or_path='BELLE-2/Belle-distilwhisper-large-v2-zh')
     r = m.recognize_speech_from_file(os.path.join(pwd_path, 'tushuguan.wav'))
     print('语音识别结果：', r)
